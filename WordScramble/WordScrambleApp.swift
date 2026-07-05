544463c9edd7ad9742cb380fbd9a119398c02fe7
ÄTAGWE ROGER <atagweroger2@gmail.com>
2026-07-05 06:52:14 +0100

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Atagwe Roger on 05/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
