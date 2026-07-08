1b53fa3008e501969ce74ecaab2d72f7a4ff5f22
ÄTAGWE ROGER <atagweroger2@gmail.com>
2026-07-08 10:52:51 +0100

    Learnt about Swift-ui URL which can be use to get files in specific location in our program or ecternal source like web

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -10,23 +10,21 @@ import SwiftUI
 struct ContentView: View {
     let people = ["Finn", "leah", "Luke", "Rey"]
     var body: some View {
-        List {
-            Text("Static Row")
-            ForEach(people , id: \.self) { name in
-                Text(name)
-            }
-            Text("Sttic Row")
                 
             
         }
+    
+    func testBundles() {
+        if let fileUrl = Bundle.main.url(forResource: "somefile", withExtension: "txt") {
+            if let fileContent = try? String(contentsOf: fileUrl) {
+                
+            }
+        }
+    }
+
             
     }
-    func testStrings() -> [String] {
-        let input = "a b c"
-        let letters = input.components(separatedBy: " ")
-        return letters
-    }
-}
+
 
 #Preview {
     ContentView()
